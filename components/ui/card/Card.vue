e7a1fab3d5fd8e89e18fed439adf1e9cddb7369d
Taziiev Andrii <andrewtaziiev@gmail.com>
2024-06-09 09:06:53 +0300

    started created main block

---
+++ components/ui/card/Card.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+</script>
+
+<template>
+  <div
+    :class="
+      cn(
+        'rounded-lg border bg-card text-card-foreground animation border-transparent transition-colors hover:border-[#a252c83d]',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+@keyframes show {
+  from {
+    @apply border-[#a252c83d];
+    transform: scale(0.5) translateY(-30px);
+    opacity: 0.4;
+  }
+  90% {
+    @apply border-[#a252c83d];
+  }
+  to {
+    @apply border-transparent;
+  }
+}
+
+.animation {
+  animation: show 0.3s ease-in-out;
+}
+</style>
