324f7056e1ae60c7c1e44695f5a3148c7ef5391f
Taziiev Andrii <andrewtaziiev@gmail.com>
2024-06-11 09:10:04 +0300

    created columns

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,20 @@
     <h1 class="font-bold text-2xl mb-10">CRM System</h1>
     <div v-if="isLoading">Loading...</div>
     <div v-else>
-      <UiCard class="mb-3" draggable="true">
-        <UiCardHeader role="button">Name Card</UiCardHeader>
-        <UiCardContent>Company</UiCardContent>
-        <UiCardFooter>Date</UiCardFooter>
-      </UiCard>
+      <div class="grid grid-cols-5 gap-16">
+        <div v-for="(column,index) in data" :key="column.id">
+          <div class="rounded bg-slate-700 py-1 px-5 mb-2 text-center">
+            {{ column.name }}
+          </div>
+          <div>
+            <UiCard class="mb-3" draggable="true" v-for="card in column.items" :key="card.id">
+              <UiCardHeader role="button">{{card.name}}</UiCardHeader>
+              <UiCardContent>Company</UiCardContent>
+              <UiCardFooter>Date</UiCardFooter>
+            </UiCard>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -23,8 +32,7 @@ useSeoMeta({
 const dragCardRef = ref<ICard | null>(null)
 const sourceColumnRef = ref<IColumn | null>(null)
 
-const {data,isLoading,refetch} = useKanbanQuery()
-
+const {data, isLoading, refetch} = useKanbanQuery()
 
 
 </script>
